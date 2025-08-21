8a99cfae2abed41ee7505ffbe0ec5c2ca994057d
Parmeshwar Gurlewad <114818920+param7666@users.noreply.github.com>
2025-08-22 00:55:51 +0530

    Add files via upload

---
+++ BootProj5-miniProject-profile/src/main/java/com/param/BootProj5MiniProjectProfileApplication.java
@@ -0,0 +1,13 @@
+package com.param;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootProj5MiniProjectProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootProj5MiniProjectProfileApplication.class, args);
+	}
+
+}
