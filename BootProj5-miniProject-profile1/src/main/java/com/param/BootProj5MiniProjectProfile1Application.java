ec2be9e1924849c35850bdf989b4d73b655a3587
Parmeshwar Gurlewad <114818920+param7666@users.noreply.github.com>
2025-09-01 11:35:55 +0530

    Add files via upload

---
+++ BootProj5-miniProject-profile1/src/main/java/com/param/BootProj5MiniProjectProfile1Application.java
@@ -0,0 +1,13 @@
+package com.param;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootProj5MiniProjectProfile1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootProj5MiniProjectProfile1Application.class, args);
+	}
+
+}
---
+++ BootProj5-miniProject-profile1/src/test/java/com/param/BootProj5MiniProjectProfile1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.param;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootProj5MiniProjectProfile1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
