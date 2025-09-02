aba5c40e3ca96d73ec353491b2ab620b92244167
Parmeshwar Gurlewad <114818920+param7666@users.noreply.github.com>
2025-09-03 01:27:43 +0530

    Add files via upload

---
+++ BootProj6-miniProject-profile-1/src/main/java/com/nt/BootProj6MiniProjectProfile1Application.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootProj6MiniProjectProfile1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootProj6MiniProjectProfile1Application.class, args);
+	}
+
+}
